2f7a9aa82b10fd58847aa583b472df30c3af30ba
Tcricco <tcricco@gmail.com>
2021-05-30 12:58:52 -0300

    Melhorias na prototipação das telas de login, cadastro e principal e evolução do algoritmo de jogo

--- backup.sql
+++ backup.sql
@@ -1,133 +1,17 @@
-CREATE TABLE IF NOT EXISTS "placas" (
-	"codigo"	INTEGER NOT NULL,
-	"descricao"	TEXT,
-	"arquivo"	TEXT,
-	PRIMARY KEY("codigo" AUTOINCREMENT)
-);
+CREATE TABLE IF NOT EXISTS placa (
+  codigo INTEGER  NOT NULL  ,
+  descricao TEXT    ,
+  botao TEXT    ,
+  arquivo TEXT      ,
+PRIMARY KEY(codigo));
 
-insert into placas(descricao,arquivo) values('A-1a - Curva Acentuada à Esquerda','Brasil_A-1a.svg.png');
-insert into placas(descricao,arquivo) values('A-1b - Curva Acentuada à Direita','Brasil_A-1b.svg.png');
-insert into placas(descricao,arquivo) values('A-2a - Curva à Esquerda','Brasil_A-2a.svg.png');
-insert into placas(descricao,arquivo) values('A-2b - Curva à Direita','Brasil_A-2b.svg.png');
-insert into placas(descricao,arquivo) values('A-3a - Pista sinuosa à Esquerda','Brasil_A-3a.svg.png');
-insert into placas(descricao,arquivo) values('A-3b - Pista sinuosa à Direita','Brasil_A-3b.svg.png');
-insert into placas(descricao,arquivo) values('A-4a - Curva Acentuada em "S" à Esquerda','Brasil_A-4a.svg.png');
-insert into placas(descricao,arquivo) values('A-4b - Curva Acentuada em "S" à Direita','Brasil_A-4b.svg.png');
-insert into placas(descricao,arquivo) values('A-5a - Curva em "S" à Esquerda','Brasil_A-5a.svg.png');
-insert into placas(descricao,arquivo) values('A-5b - Curva em "S" à Direita','Brasil_A-5b.svg.png');
-insert into placas(descricao,arquivo) values('A-6 - Cruzamento','Brasil_A-6.svg.png');
-insert into placas(descricao,arquivo) values('A-7 - Entroncamento à Esquerda','Brasil_A-7a.svg.png');
-insert into placas(descricao,arquivo) values('A-7b - Entroncamento à Direita','Brasil_A-7b.svg.png');
-insert into placas(descricao,arquivo) values('A-8 - Interseção em T','Brasil_A-8.svg.png');
-insert into placas(descricao,arquivo) values('A-9 - Bifurcação em Y','Brasil_A-9.svg.png');
-insert into placas(descricao,arquivo) values('A-10a - Entroncamento Oblíquo à Esquerda','Brasil_A-10a.svg.png');
-insert into placas(descricao,arquivo) values('A-10b - Entroncamento Oblíquo à Direita','Brasil_A-10b.svg.png');
-insert into placas(descricao,arquivo) values('A-11a - Junções Contrárias - Primeira à Esquerda','Brasil_A-11a.svg.png');
-insert into placas(descricao,arquivo) values('A-11b - Junções Contrárias - Primeira à Direita','Brasil_A-11b.svg.png');
-insert into placas(descricao,arquivo) values('A-12 - Carrossel','Brasil_A-12.svg.png');
-insert into placas(descricao,arquivo) values('A-13a - Confluência à Esquerda','Brasil_A-13a.svg.png');
-insert into placas(descricao,arquivo) values('A-13a - Confluência à Direita','Brasil_A-13b.svg.png');
-insert into placas(descricao,arquivo) values('A-14 - Semáforo à Frente','Brasil_A-14.svg.png');
-insert into placas(descricao,arquivo) values('A-15 - Parada à Frente','Brasil_A-15.svg.png');
-insert into placas(descricao,arquivo) values('A-16 - Bonde','Brasil_A-16.svg.png');
-insert into placas(descricao,arquivo) values('A-17 - Pista Irregular','Brasil_A-17.svg.png');
-insert into placas(descricao,arquivo) values('A-18 - Saliência ou Lombada','Brasil_A-18.svg.png');
-insert into placas(descricao,arquivo) values('A-19 - Depressão','Brasil_A-19.svg.png');
-insert into placas(descricao,arquivo) values('A-20a - Declive Acentuado','Brasil_A-20a.svg.png');
-insert into placas(descricao,arquivo) values('A-20b - Aclive Acentuado','Brasil_A-20b.svg.png');
-insert into placas(descricao,arquivo) values('A-21a - Estreitamento de Pista ao Centro','Brasil_A-21a.svg.png');
-insert into placas(descricao,arquivo) values('A-21b - Estreitamento de Pista à Esquerda','Brasil_A-21b.svg.png');
-insert into placas(descricao,arquivo) values('A-21c - Estreitamento de Pista à Direita','Brasil_A-21c.svg.png');
-insert into placas(descricao,arquivo) values('A-21d - Alargamento de Pista à Esquerda','Brasil_A-21d.svg.png');
-insert into placas(descricao,arquivo) values('A-21e - Alargamento de Pista à Direita','Brasil_A-21e.svg.png');
-insert into placas(descricao,arquivo) values('A-22 - Ponte Estreita','Brasil_A-22.svg.png');
-insert into placas(descricao,arquivo) values('A-23 - Ponte móvel','Brasil_A-23.svg.png');
-insert into placas(descricao,arquivo) values('A-24 - Obras','Brasil_A-24.svg.png');
-insert into placas(descricao,arquivo) values('A-25 - Mão Dupla à Frente','Brasil_A-25.svg.png');
-insert into placas(descricao,arquivo) values('A-26a - Sentido Único','Brasil_A-26a.svg.png');
-insert into placas(descricao,arquivo) values('A-26b - Sentido Duplo','Brasil_A-26b.svg.png');
-insert into placas(descricao,arquivo) values('A-27 - Área com Desmoronamento','Brasil_A-27.svg.png');
-insert into placas(descricao,arquivo) values('A-28 - Pista Escorregadia','Brasil_A-28.svg.png');
-insert into placas(descricao,arquivo) values('A-29 - Projeção de Cascalho','Brasil_A-29.svg.png');
-insert into placas(descricao,arquivo) values('A-30a - Trânsito de Ciclistas','Brasil_A-30a.svg.png');
-insert into placas(descricao,arquivo) values('A-30b - Passagem Sinalizada de Ciclistas','Brasil_A-30b.svg.png');
-insert into placas(descricao,arquivo) values('A-30c - Trânsito de Ciclistas e Pedestres','Brasil_A-30c.svg.png');
-insert into placas(descricao,arquivo) values('A-31 - Trânsito de Tratores','Brasil_A-31.svg.png');
-insert into placas(descricao,arquivo) values('A-32a - Passagem de Pedestres','Brasil_A-32a.svg.png');
-insert into placas(descricao,arquivo) values('A-32b - Passagem Sinalizada de Pedestres','Brasil_A-32b.svg.png');
-insert into placas(descricao,arquivo) values('A-33a - Área Escolar','Brasil_A-33a.svg.png');
-insert into placas(descricao,arquivo) values('A-33b - Passagem Sinalizada de Escolares','Brasil_A-33b.svg.png');
-insert into placas(descricao,arquivo) values('A-34 - Crianças','Brasil_A-34.svg.png');
-insert into placas(descricao,arquivo) values('A-35 - Animais','Brasil_A-35.svg.png');
-insert into placas(descricao,arquivo) values('A-36 - Animais selvagens','Brasil_A-36.svg.png');
-insert into placas(descricao,arquivo) values('A-37 - Altura limitada','Brasil_A-37.svg.png');
-insert into placas(descricao,arquivo) values('A-38 - Largura limitada','Brasil_A-38.svg.png');
-insert into placas(descricao,arquivo) values('A-39 - Passagem de Nível sem Barreira','Brasil_A-39.svg.png');
-insert into placas(descricao,arquivo) values('A-40 - Passagem de Nível com Barreira','Brasil_A-40.svg.png');
-insert into placas(descricao,arquivo) values('A-41 - Cruz de Santo André','Brasil_A-41b.svg.png');
-insert into placas(descricao,arquivo) values('A-42a - Início de Pista Dupla','Brasil_A-42a.svg.png');
-insert into placas(descricao,arquivo) values('A-42b - Fim de Pista Dupla','Brasil_A-42b.svg.png');
-insert into placas(descricao,arquivo) values('A-42c - Pista Dividida','Brasil_A-42c.svg.png');
-insert into placas(descricao,arquivo) values('A-43 - Aeroporto','Brasil_A-43.svg.png');
-insert into placas(descricao,arquivo) values('A-44 - Vento Lateral','Brasil_A-44.svg.png');
-insert into placas(descricao,arquivo) values('A-45 - Rua sem Saída','Brasil_A-45.svg.png');
-insert into placas(descricao,arquivo) values('A-46 - Peso Bruto Total Limitado','Brasil_A-46.svg.png');
-insert into placas(descricao,arquivo) values('A-47 - Peso Limitado por Eixo','Brasil_A-47.svg.png');
-insert into placas(descricao,arquivo) values('A-48 - Comprimento limitado','Brasil_A-48.svg.png');
-insert into placas(descricao,arquivo) values('R-1 - Parada obrigatória','Brasil_R-1.svg.png');
-insert into placas(descricao,arquivo) values('R-2 - Dê a preferência','Brasil_R-2.svg.png');
-insert into placas(descricao,arquivo) values('R-3 - Sentido proibido (Não entre)','Brasil_R-3.svg.png');
-insert into placas(descricao,arquivo) values('R-4a - Proibido virar à esquerda','Brasil_R-4a.svg.png');
-insert into placas(descricao,arquivo) values('R-4b - Proibido virar à direita','Brasil_R-4b.svg.png');
-insert into placas(descricao,arquivo) values('R-5a - Proibido retornar à esquerda','Brasil_R-5a.svg.png');
-insert into placas(descricao,arquivo) values('R-5b - Proibido retornar à direita','Brasil_R-5b.svg.png');
-insert into placas(descricao,arquivo) values('R-6a - Proibido estacionar','Brasil_R-6a.svg.png');
-insert into placas(descricao,arquivo) values('R-6b - Estacionamento regulamentado','Brasil_R-6b.svg.png');
-insert into placas(descricao,arquivo) values('R-6c - Proibido parar e estacionar','Brasil_R-6c.svg.png');
-insert into placas(descricao,arquivo) values('R-7 - Proibido ultrapassar','Brasil_R-7.svg.png');
-insert into placas(descricao,arquivo) values('R-8a - Proibido mudar de faixa para a direita','Brasil_R-8a.svg.png');
-insert into placas(descricao,arquivo) values('R-8b - Proibido mudar de faixa para a esquerda','Brasil_R-8b.svg.png');
-insert into placas(descricao,arquivo) values('R-9 - Proibido trânsito de caminhões','Brasil_R-9.svg.png');
-insert into placas(descricao,arquivo) values('R-10 - Proibido trânsito de automóveis','Brasil_R-10.svg.png');
-insert into placas(descricao,arquivo) values('R-11 - Proibido trânsito de veículos de tração animal','Brasil_R-11.svg.png');
-insert into placas(descricao,arquivo) values('R-12 - Proibido trânsito de bicicletas','Brasil_R-12.svg.png');
-insert into placas(descricao,arquivo) values('R-13 - Proibido trânsito de tratores','Brasil_R-13.svg.png');
-insert into placas(descricao,arquivo) values('R-14 - Peso bruto total máximo permitido','Brasil_R-14.svg.png');
-insert into placas(descricao,arquivo) values('R-15 - Altura máxima permitida','Brasil_R-15.svg.png');
-insert into placas(descricao,arquivo) values('R-16 - Largura máxima permitida','Brasil_R-16.svg.png');
-insert into placas(descricao,arquivo) values('R-17 - Peso máximo permitido por eixo','Brasil_R-17.svg.png');
-insert into placas(descricao,arquivo) values('R-18 - Comprimento máximo permitido','Brasil_R-18.svg.png');
-insert into placas(descricao,arquivo) values('R-19 - Velocidade máxima permitida','Brasil_R-19.svg.png');
-insert into placas(descricao,arquivo) values('R-20 - Proibido acionar buzina ou sinal sonoro','Brasil_R-20.svg.png');
-insert into placas(descricao,arquivo) values('R-21 - Alfândega','Brasil_R-21.svg.png');
-insert into placas(descricao,arquivo) values('R-22 - Uso obrigatório de corrente','Brasil_R-22.svg.png');
-insert into placas(descricao,arquivo) values('R-23 - Conserve-se à direita','Brasil_R-23.svg.png');
-insert into placas(descricao,arquivo) values('R-24a - Sentido obrigatório','Brasil_R-24a.svg.png');
-insert into placas(descricao,arquivo) values('R-24b - Passagem obrigatória','Brasil_R-24b.svg.png');
-insert into placas(descricao,arquivo) values('R-25a - Vire à esquerda','Brasil_R-25a.svg.png');
-insert into placas(descricao,arquivo) values('R-25b - Vire à direita','Brasil_R-25b.svg.png');
-insert into placas(descricao,arquivo) values('R-25c - Siga em frente ou à esquerda','Brasil_R-25c.svg.png');
-insert into placas(descricao,arquivo) values('R-25d - Siga em frente ou à direita','Brasil_R-25d.svg.png');
-insert into placas(descricao,arquivo) values('R-26 - Siga em frente','Brasil_R-26.svg.png');
-insert into placas(descricao,arquivo) values('R-27 - Veículos pesados mantenham-se à direita','Brasil_R-27.svg.png');
-insert into placas(descricao,arquivo) values('R-28 - Sentido duplo','Brasil_R-28.svg.png');
-insert into placas(descricao,arquivo) values('R-29 - Proibido trânsito de pedestres','Brasil_R-29.svg.png');
-insert into placas(descricao,arquivo) values('R-30 - Pedestre, ande pela esquerda','Brasil_R-30.svg.png');
-insert into placas(descricao,arquivo) values('R-31 - Pedestre, ande pela direita','Brasil_R-31.svg.png');
-insert into placas(descricao,arquivo) values('R-32 - Circulação exclusiva de ônibus','Brasil_R-32.svg.png');
-insert into placas(descricao,arquivo) values('R-33 - Rotatória','Brasil_R-33.svg.png');
-insert into placas(descricao,arquivo) values('R-34 - Circulação exclusiva de bicicletas','Brasil_R-34.svg.png');
-insert into placas(descricao,arquivo) values('R-35a - Ciclista, transite à esquerda','Brasil_R-35a.svg.png');
-insert into placas(descricao,arquivo) values('R-35b - Ciclista, transite à direita','Brasil_R-35b.svg.png');
-insert into placas(descricao,arquivo) values('R-36a - Ciclistas à esquerda, pedestres à direita','Brasil_R-36a.svg.png');
-insert into placas(descricao,arquivo) values('R-36b - Pedestres à esquerda, ciclistas à direita','Brasil_R-36b.svg.png');
-insert into placas(descricao,arquivo) values('R-37 - Proibido trânsito de motocicletas, motonetas e ciclomotores','Brasil_R-37.svg.png');
-insert into placas(descricao,arquivo) values('R-38 - Proibido trânsito de ônibus','Brasil_R-38.svg.png');
-insert into placas(descricao,arquivo) values('R-39 - Circulação exclusiva de caminhões','Brasil_R-39.svg.png');
-insert into placas(descricao,arquivo) values('R-40 - Trânsito proibido a carros de mão','Brasil_R-40.svg.png');
-insert into placas(descricao,arquivo) values('Use o cinto de segurança','Use_o_cinto_br_placa_educativa.png');
-insert into placas(descricao,arquivo) values('Obedeça a sinalização','Obedece_a_sinalizacao_br_placa_educativa.png');
-insert into placas(descricao,arquivo) values('Não jogue lixo na rodovia. Natureza agradece','Nao_jogue_lixo_rs_placa_educativa.png');
-insert into placas(descricao,arquivo) values('Ajude a cuidar do patrimônio natural. Dirija com atenção','Cuidar_do_patrimonio_natural_rs_placa_educativa.png');
-insert into placas(descricao,arquivo) values('Preserve a natureza. pense nas gerações futuras','Preserve_a_natureza_rs_placa_educativa.png');
+
+
+CREATE TABLE IF NOT EXISTS parametro (
+  codigo INTEGER  NOT NULL  ,
+  nome TEXT    ,
+  valor TEXT      ,
+PRIMARY KEY(codigo));
+
+
 
